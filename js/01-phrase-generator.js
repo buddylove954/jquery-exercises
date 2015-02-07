fb0fa0e9c016131bf29f3d1c220e8046feca448c
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-02-06 21:12:27 -0500

    Add instructions for phrase generator exercise

---
+++ js/01-phrase-generator.js
@@ -0,0 +1,40 @@
+/* --------------------------------------------------------------------------
+   Task #0
+
+   Fill the `phrases` array with some of your favorite phrases.
+   -------------------------------------------------------------------------- */
+
+var phrases = [
+    'I like pie.',
+    'Add some of your own phrases here.',
+    'Make sure to follow all but the last phrase with a comma.'
+];
+
+
+/* --------------------------------------------------------------------------
+   Task #1
+
+   Use jQuery to make it so that when you click the refresh button, the phrase
+   currently displayed on the screen is replaced with the next one from
+   the `phrases` array. When the last phrase is reached, it should start again
+   from the beginning.
+   -------------------------------------------------------------------------- */
+
+
+/* --------------------------------------------------------------------------
+   Task #2
+
+   Notice how the cursor changes when you move your mouse over the phrase. Again
+   use jQuery to make it so that when you click on the phrase itself it behaves
+   as if the refresh button was clicked.
+   -------------------------------------------------------------------------- */
+
+
+/* --------------------------------------------------------------------------
+   Task #3
+
+   Use jQuery to make it so that when you click the trash button, the phrase
+   changes but the phrase that was just removed is also removed from
+   circulation. It shouldn't appear again no matter how many times the phrase is
+   changed.
+   -------------------------------------------------------------------------- */
